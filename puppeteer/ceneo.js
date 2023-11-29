40a8b4ebae97835f1afa589d8b579896e3b0f0a0
jakubg123 <jakubgniadek87@gmail.com>
2023-11-29 16:39:04 +0100

    title, price, rating collecting

--- puppeteer/ceneo.js
+++ puppeteer/ceneo.js
@@ -10,7 +10,7 @@ puppeteer.launch({
     userDataDir: "./tmp"
 }).then(async browser => {
     const page = await browser.newPage()
-    const url = process.argv[2];
+    const url = 'https://www.ceneo.pl/Karmy_dla_psow;szukaj-brit+karmy+dla+ps%c3%b3w'
     await page.goto(url);
     await page.waitForXPath('//*[@id="body"]/div/div/div[3]/div/section/div[5]', { timeout: 100000 })
 
@@ -19,39 +19,46 @@ puppeteer.launch({
     let items = [];
 
     for (const product of products) {
+        let title = "Null"
+        let price = "Null"
+        let rating = "Null"
         try {
-            const title = await page.evaluate(
+            // strong > a > span
+            title = await page.evaluate(
                 el => el.querySelector("strong > a > span").textContent,
                 product
             );
-            items.push(new Product(title));
-            console.log(title);
         } catch (error) {
             console.error("Error extracting product title:", error.message);
         }
+
         try {
-            const price = await page.evaluate(
-                el => el.querySelector("strong > a > span").textContent,
+            price = await page.evaluate(
+                el => el.querySelector("span.price-format.nowrap > span > span.value").textContent,
                 product
             );
-            items.push(new Product(title));
-            console.log(title);
         } catch (error) {
             console.error("Error extracting product title:", error.message);
         }
+
         try {
-            const title = await page.evaluate(
-                el => el.querySelector("strong > a > span").textContent,
+            rating = await page.evaluate(
+                el => el.querySelector("div.prod-review > span > span.product-score").textContent,
                 product
             );
-            items.push(new Product(title));
-            console.log(title);
+
+            rating = rating.substring(1, 4);
+
         } catch (error) {
             console.error("Error extracting product title:", error.message);
         }
+
+        console.log(title)
+        console.log(price)
+        console.log(rating)
+
     }
 
-    console.log(items);
 
     await browser.close();
 });
