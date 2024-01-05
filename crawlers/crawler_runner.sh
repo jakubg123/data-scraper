de778a3ae214f8881c47a36d3e0dedf072fb3e04
jakubg123 <jakubgniadek87@gmail.com>
2024-01-05 05:09:46 +0100

    refactor

--- crawlers/crawler_runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-scrape_phrase="karma dla psa brit 15kg"
-
-node $HOME/data-scraper/crawlers/ceneo_scraper.js "$scrape_phrase"
-python $HOME/data-scraper/crawlers/olx.py "$scrape_phrase"
